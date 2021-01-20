6febb748198c96ad66ad7a0be2dac68bdb7e7cbe
Khesraw Mohammadi <KhesrawMohammadi@outlook.com>
2021-01-20 15:00:18 +0330

    setCenter Method is added in Tools Class

--- src/datastructurefinalproject/Tools.java
+++ src/datastructurefinalproject/Tools.java
@@ -1,10 +1,23 @@
 package datastructurefinalproject;
 
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+
 /**
  *
  * @author mohammadi
  */
 public class Tools 
 {
-    
+    public void setCenter(JFrame jframe)
+    {
+            Toolkit tool = Toolkit.getDefaultToolkit();
+            int width = jframe.getWidth();
+            int height = jframe.getHeight();
+            int screenWidth = tool.getScreenSize().width;
+            int screenHeight = tool.getScreenSize().height;
+            int x = (screenWidth - width)/2;
+            int y = (screenHeight-height)/2;
+            jframe.setLocation(x, y);
+    }
 }
